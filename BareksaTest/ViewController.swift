c17e5ae0ff4f8ced90790a27796f7918a44df9ef
denny.arfansyah <denny.arfansyah@amarbank.co.id>
2022-07-05 12:01:14 +0700

    Initial Commit

---
+++ BareksaTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BareksaTest
+//
+//  Created by Denny Arfansyah on 05/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
